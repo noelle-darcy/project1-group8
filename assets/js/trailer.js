6fcdfaa20a8e58901c26a28e0d479a3926536512
Allison <farrella95@gmail.com>
2022-09-29 13:28:43 -0400

    integrated the trailer API

--- assets/js/trailer.js
+++ assets/js/trailer.js
@@ -1,11 +1,3 @@
-var submitBtn = document.querySelector(".generateBtn");
-
-function generate(e) {
-  window.location.href = "p2.index.html";
-}
-
-addEventListener("click", generate);
-
 const options = {
   method: "GET",
   headers: {
